1030f5b8196c22bd7b2c83e5f77226850de630a4
praveen <pkuninti@praveens-mbp.fios-router.home>
2019-09-01 12:00:41 -0400

    Fix CORS headers on APIGateway later

--- src/main/java/com/idexcel/adminservice/controller/LenderController.java
+++ src/main/java/com/idexcel/adminservice/controller/LenderController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import com.idexcel.adminservice.service.LenderService;
 
 @RestController
 @RequestMapping("idexceldemo/lenders")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LenderController {
 	
 	
