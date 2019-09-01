b6dc1c187d22d0f7ccc478a21c7ed8094556a91f
praveen <pkuninti@praveens-mbp.fios-router.home>
2019-09-01 11:53:07 -0400

    Remove CORS headers from SpringBott and handle in API Gateway

--- src/main/java/com/idexcel/adminservice/controller/LenderController.java
+++ src/main/java/com/idexcel/adminservice/controller/LenderController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +31,6 @@ import com.idexcel.adminservice.service.LenderService;
 
 @RestController
 @RequestMapping("idexceldemo/lenders")
-@CrossOrigin(origins = "http://localhost:4200")
 public class LenderController {
 	
 	
